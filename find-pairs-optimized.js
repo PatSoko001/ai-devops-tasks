2fa2b80e5e09208978373440b514be68aac8949f
PatSoko001 <patsoko.01@interia.pl>
2025-05-25 17:22:32 +0200

    Create find-pairs-optimized.js

---
+++ find-pairs-optimized.js
@@ -0,0 +1,14 @@
+function findPairsOptimized(arr, targetSum) {
+  const seen = new Set();
+  const pairs = [];
+
+  for (const num of arr) {
+    const complement = targetSum - num;
+    if (seen.has(complement)) {
+      pairs.push([complement, num]);
+    }
+    seen.add(num);
+  }
+
+  return pairs;
+}
